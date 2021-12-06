e49f92a16414ec51109289dd0c37f619aed9527b
TomKing062 <yx42_44@126.com>
2021-12-06 14:06:44 +0800

    Use default browser

--- starira.bat
+++ starira.bat
@@ -1,12 +1,12 @@
 adb shell starira_res
-for /f %%i in ('type emulator_share\strr_banner.txt') do (start chrome.exe https://cdn.starira.xyz/api/assets/jp/res_ja/res/banner/%%i)
-for /f %%i in ('type emulator_share\strr_event.txt') do (start chrome.exe https://cdn.starira.xyz/api/assets/jp/res_ja/res/event_permanent/banner/%%i)
-for /f %%i in ('type emulator_share\strr_gacha.txt') do (start chrome.exe https://cdn.starira.xyz/api/assets/jp/res_ja/res/gacha/background/%%i)
+for /f %%i in ('type emulator_share\strr_banner.txt') do (start https://cdn.starira.xyz/api/assets/jp/res_ja/res/banner/%%i)
+for /f %%i in ('type emulator_share\strr_event.txt') do (start https://cdn.starira.xyz/api/assets/jp/res_ja/res/event_permanent/banner/%%i)
+for /f %%i in ('type emulator_share\strr_gacha.txt') do (start https://cdn.starira.xyz/api/assets/jp/res_ja/res/gacha/background/%%i)
 for /f %%i in ('type emulator_share\strr_card.txt') do (
-start chrome.exe https://karth.top/dress/%%i?global=1
-start chrome.exe https://karth.top/api/adventure/ww/30%%i1.json
-start chrome.exe https://karth.top/api/adventure/ww/30%%i2.json
-start chrome.exe https://karth.top/api/adventure/ww/31%%i1.json
-start chrome.exe https://karth.top/api/adventure/ww/31%%i2.json
+start https://karth.top/dress/%%i?global=1
+start https://karth.top/api/adventure/ww/30%%i1.json
+start https://karth.top/api/adventure/ww/30%%i2.json
+start https://karth.top/api/adventure/ww/31%%i1.json
+start https://karth.top/api/adventure/ww/31%%i2.json
 )
-for /f %%i in ('type emulator_share\strr_story.txt') do (start chrome.exe https://karth.top/adventure/%%i?global=1)
+for /f %%i in ('type emulator_share\strr_story.txt') do (start https://karth.top/adventure/%%i?global=1)
