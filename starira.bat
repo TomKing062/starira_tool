026eaa24c7a8850c1d36997b2ff350f471b661f2
TomKing062 <yx42_44@126.com>
2022-09-29 19:28:27 +0800

    sync api change

--- starira.bat
+++ starira.bat
@@ -1,7 +1,7 @@
 adb shell starira_res
-for /f %%i in ('type emulator_share\strr_banner.txt') do (start https://cdn.starira.xyz/api/assets/jp/res_ja/res/banner/%%i)
-for /f %%i in ('type emulator_share\strr_event.txt') do (start https://cdn.starira.xyz/api/assets/jp/res_ja/res/event_permanent/banner/%%i)
-for /f %%i in ('type emulator_share\strr_gacha.txt') do (start https://cdn.starira.xyz/api/assets/jp/res_ja/res/gacha/background/%%i)
+for /f %%i in ('type emulator_share\strr_banner.txt') do (start https://cdn.karth.top/api/assets/jp/res_ja/res/banner/%%i)
+for /f %%i in ('type emulator_share\strr_event.txt') do (start https://cdn.karth.top/api/assets/jp/res_ja/res/event_permanent/banner/%%i)
+for /f %%i in ('type emulator_share\strr_gacha.txt') do (start https://cdn.karth.top/api/assets/jp/res_ja/res/gacha/background/%%i)
 for /f %%i in ('type emulator_share\strr_card.txt') do (
 start https://karth.top/dress/%%i?global=1
 start https://karth.top/api/adventure/ww/30%%i1.json
